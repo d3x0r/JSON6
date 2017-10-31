26ec6570f0edcda3e4975a6b19a2702a7f11754a
d3x0r <d3x0r@users.noreply.github.com>
2017-10-31 08:26:50 -0700

    Add number parsing failure tests

--- tests/numberTest.js
+++ tests/numberTest.js
@@ -19,6 +19,8 @@ console.log( "typeof( n ) =", typeof n, n );
 var n = 0x123;
 console.log( "typeof( n ) =", typeof n, n );
 
+console.log( '-------' );
+
 var n = JSON.parse( '.123' );
 console.log( "typeof( n ) =", typeof n, n );
 
@@ -36,3 +38,21 @@ console.log( "typeof( n ) =", typeof n, n );
 
 var n = JSON.parse( '0x123' );
 console.log( "typeof( n ) =", typeof n, n );
+
+function failSuccess( string ) {
+	try {
+		var n = JSON.parse( string );
+		console.log( "typeof( n ) =", typeof n, n );
+	} catch(err) {
+		console.log( "Expected failure :", err.message );
+	}
+}
+
+failSuccess( ".123-45" );
+failSuccess( ".123e2-45" );
+failSuccess( ".123e--45" );
+failSuccess( ".123e+-45" );
+failSuccess( ".123e3-45" );
+failSuccess( ".05x23" );
+failSuccess( "0xx23" );
+failSuccess( "0x23.45" );
