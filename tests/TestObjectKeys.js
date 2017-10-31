bb1e09400bae70e5691573a572e1285d40ec093a
d3x0r <d3x0r@users.noreply.github.com>
2017-10-31 06:09:39 -0700

    Extend test cases

--- tests/TestObjectKeys.js
+++ tests/TestObjectKeys.js
@@ -1,6 +1,8 @@
 
 var JSON6 = require( ".." );
 
+
+
 try {
 	var result = JSON6.parse( "{ my- key:3}" );
 	console.log( "result:", result );
@@ -15,8 +17,74 @@ try {
 	console.log( 'error expected:', err.message );
 }
 
+try {
+	var result = JSON6.parse( "{ my-key { failure:true}:3}" );
+	console.log( "result:", result );
+} catch( err ) {
+	console.log( 'error expected:', err.message );
+}
+
+try {
+	var result = JSON6.parse( "{ { my-key:3 } }" );
+	console.log( "result:", result );
+} catch( err ) {
+	console.log( 'error expected:', err.message );
+}
+
+try {
+	var result = JSON6.parse( "{ [ my-key:3 } }" );
+	console.log( "result:", result );
+} catch( err ) {
+	console.log( 'error expected:', err.message );
+}
+
+try {
+	var result = JSON6.parse( "{ my-key[:3 } }" );
+	console.log( "result:", result );
+} catch( err ) {
+	console.log( 'error expected:', err.message );
+}
+
+try {
+	var result = JSON6.parse( "{ my-key]:3 } }" );
+	console.log( "result:", result );
+} catch( err ) {
+	console.log( 'error expected:', err.message );
+}
+
+
+
 var result = JSON6.parse( "{ 'my  -  key':3}" );
 console.log( "result:", result );
 
 var result = JSON6.parse( "{ my-key\\m&m+*|:3}" );
 console.log( "result:", result );
+
+var result = JSON6.parse( "{ my-key //test \n :3}" );
+console.log( "result:", result );
+
+var result = JSON6.parse( "{ my-key /*test */ :3}" );
+console.log( "result:", result );
+
+
+
+
+/*
+
+{ my- key:3} // fault
+{ my -  key:3}  // fault
+{ my-key { failure:true}:3} // fault
+{ { my-key:3 } }   // fault
+{ [ my-key:3 } }   // fault
+{ my-key[:3 } }    // fault
+{ my-key]:3 } }    // fault
+
+{ 'my  -  key':3}   // valid
+{ my-key\\m&m+*|:3}  // valid
+
+{ my-key //test 
+   :3}   // valid
+
+{ my-key /*test * / :3}  // valid
+
+*/
