d02d54c60a9d708b2fe613aab639bfe7b9f0cf5a
Brett Zamir <brettz9@yahoo.com>
2020-04-05 11:56:09 +0800

    - Testing: Improve coverage (JSON6.escape, string escapes, ignore Node-unreachable)

---
+++ test/escape.js
@@ -0,0 +1,12 @@
+var JSON6 = require( ".." )
+
+describe('JSON6.escape', function () {
+    it('Escapes', function () {
+        var str = JSON6.escape('a"b\\c`d\'e');
+        expect(str).to.equal('a\\"b\\\\c\\`d\\\'e');
+    });
+    it('Handles empty string', function () {
+        var str = JSON6.escape('');
+        expect(str).to.equal('');
+    });
+});
---
+++ test/stringEscapes.js
@@ -0,0 +1,41 @@
+var JSON6 = require( '..' );
+
+describe('String escapes', function () {
+    describe('Octal escapes', function () {
+        it('Parses string octal escape', function () {
+    		var result = JSON6.parse( '"\\056"' );
+    		expect(result).to.deep.equal('.');
+    	});
+    });
+    describe('Unicode wide escapes', function () {
+        it('Parses Unicode wide escape (lower-case)', function () {
+            var result = JSON6.parse( '"\\u{002e}"' );
+    		expect(result).to.deep.equal('.');
+        });
+        it('Parses Unicode wide escape (upper-case)', function () {
+            var result = JSON6.parse( '"\\u{002E}"' );
+    		expect(result).to.deep.equal('.');
+        });
+        it('Throws with bad Unicode wide escape (upper-case)', function () {
+            expect(function () {
+                JSON6.parse( '"\\u{00G}"' );
+            }).to.throw(Error, /escaped character, parsing hex/);
+        });
+    });
+    describe('String hex escapes', function () {
+        it('Parses string hex', function () {
+            var result = JSON6.parse( '"\\x2e"' );
+    		expect(result).to.deep.equal('.');
+        });
+    });
+    describe('Single escapes', function () {
+        it('\\b', function () {
+            var result = JSON6.parse( '"\\b"' );
+    		expect(result).to.deep.equal('\b');
+        });
+        it('\\f', function () {
+            var result = JSON6.parse( '"\\f"' );
+    		expect(result).to.deep.equal('\f');
+        });
+    });
+});
