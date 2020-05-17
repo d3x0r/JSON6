f47cfe8bb66a364c66c2beeb2ff060a52961793d
d3x0r <d3x0r@no-reply.github.com>
2020-05-17 01:23:46 -0700

    Add test case

---
+++ test/testIncompleteStringEscapes.js
@@ -0,0 +1,30 @@
+
+var JSON6 = require( ".." )
+
+var parse = JSON6.parse;
+var o;
+
+describe('Incomplete String Escape tests', function () {
+
+	expect(function () {
+		o = parse( "'\\x1'" );
+		console.log( "got back:", o );
+	}).to.throw(Error);
+
+
+	expect(function () {
+		o = parse( "'\\01'" );
+		console.log( "got back:", o );
+	}).to.throw(Error);
+
+	expect(function () {
+		o = parse( "'\\u31'" );
+		console.log( "got back:", o );
+	}).to.throw(Error);
+
+	expect(function () {
+		o = parse( "'\\u{0'" );
+		console.log( "got back:", o );
+	}).to.throw(Error);
+
+} );
