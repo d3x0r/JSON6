eac8ca77620217024ec8cdfbb765610ebd1fdcf1
d3x0r <d3x0r@no-reply.github.com>
2020-05-27 23:16:09 -0700

    comply to lint formatting

--- test/json6_internals_test.js
+++ test/json6_internals_test.js
@@ -3,29 +3,29 @@ const JSON6 = require( ".." );
 
 describe('JSON6.streaming.internal', function () {
 	it('requeues input buffers for pending inputs', function () {
-        	const results = [];
+		const results = [];
 		const parser = JSON6.begin();
-                var parseResult;
+		let parseResult;
 
-                parseResult = parser._write( "1 2 3 " );
+		parseResult = parser._write( "1 2 3 " );
 		results.push( parseResult );
 		results.push( parser.value() );
 
-                parseResult = parser._write( "1 2 3 4 5 " );
+		parseResult = parser._write( "1 2 3 4 5 " );
 		results.push( parseResult );
 		results.push( parser.value() );
 	
-                parseResult = parser._write( "1 2 3 " );
-	        parseResult = parser._write();
+		parseResult = parser._write( "1 2 3 " );
+		parseResult = parser._write();
 		results.push( parseResult );
 		results.push( parser.value() );
 
-	while( parseResult = parser._write() ) {
-		console.log( "Leftover Data:", parser.value() );
-	}
+		while( (parseResult = parser._write()) ) {
+			console.log( "Leftover Data:", parser.value() );
+		}
 		
-                // and now, there will be a 'saved' which push() can pull from.
-                parseResult = parser._write( "1 " );
+		// and now, there will be a 'saved' which push() can pull from.
+		parseResult = parser._write( "1 " );
 		console.log( "Leftover Data:", parser.value() );
 
 
