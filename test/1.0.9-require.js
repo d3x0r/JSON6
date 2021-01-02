e1a208c12ea9bc123279a261ce6d0cd5c1b4f602
d3x0r <d3x0r@no-reply.github.com>
2021-01-02 14:56:45 -0800

    Label tests better

--- test/1.0.9-require.js
+++ test/1.0.9-require.js
@@ -2,7 +2,7 @@
 require( ".." );
 //require( "../lib/require.js" );
 
-describe('Added in 1.0.9 - require', function () {
+describe('Added in 1.0.9 - require(cjs)', function () {
 	
 	it('allows using require on extension', function () {
 		const config = require( "./config.json6" );
