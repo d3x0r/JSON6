bed346c2651f30a7cbbc6cef7d23f855c78afa9a
d3x0r <d3x0r@no-reply.github.com>
2021-01-02 23:16:43 -0800

    Update test

--- test/1.0.9-require.mjs
+++ test/1.0.9-require.mjs
@@ -2,11 +2,16 @@
 import * as JSON6 from "../dist/index.mjs";
 //require( "../lib/require.js" );
 
+//import {default as config} from './1.0.9-require.json6'
+//console.log( config );
+
 describe('Added in 1.0.9 - require(esm)', function () {
 	
 	it('allows using require on extension', function () {
-		const config = import( "./1.0.9-require.json6" ).then( config=>{
-			expect( config ).to.deep.equal( {
+		console.log( "IMPORT:" );
+		return import( "./1.0.9-require.json6" ).then( config=>{
+			//console.log( "GOT:", config );
+			expect( config.default ).to.deep.equal( {
 				   desc: "configuration file to read",
 			   value:123}
 			 );
